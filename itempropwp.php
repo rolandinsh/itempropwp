e473eddf516935d9305e8bf4b6ee3944c0dc0870
rolandinsh <rolands@mediabox.lv>
2015-03-22 03:19:32 +0100

    fix #17 Fixed a typo in the plugin's description

--- itempropwp.php
+++ itempropwp.php
@@ -3,7 +3,7 @@
 /**
   Plugin Name: itemprop WP for SERP (and SEO) Rich snippets
   Plugin URI: http://simplemediacode.com/wordpress-pugins/itemprop-wp/?utm_source=wordpress&utm_medium=wpplugin&utm_campaign=itempropWP&utm_content=v-3.4.3-itempropWP_load_widgets
-  Description: Add human invisible schema.org code to conent
+  Description: Add human invisible schema.org code to content
   Version: 3.4.3
   Requires at least: 3.3
   Tested up to: 4.1.1
