6e8f3a58eda1502aabe7b289981626a812478e89
rolandinsh <rolands@mediabox.lv>
2016-04-17 22:10:13 +0300

    3.5.201604171

--- itempropwp.php
+++ itempropwp.php
@@ -2,11 +2,11 @@
 
 /**
   Plugin Name: itemprop WP for SERP (and SEO) Rich snippets
-  Plugin URI: http://simplemediacode.com/?utm_source=wordpress&utm_medium=wpplugin&utm_campaign=itempropWP&utm_content=v-3.5.201602151-itempropWP_load_widgets
+  Plugin URI: http://simplemediacode.com/?utm_source=wordpress&utm_medium=wpplugin&utm_campaign=itempropWP&utm_content=v-3.5.201604171-itempropWP_load_widgets
   Description: Add human invisible schema.org code to content
-  Version: 3.5.201602152
+  Version: 3.5.201604171
   Requires at least: 4.0
-  Tested up to: 4.4.2
+  Tested up to: 4.5
   Author: Rolands Umbrovskis
   Author URI: http://umbrovskis.com
   License: simplemediacode
@@ -27,7 +27,7 @@ if (!function_exists('add_action')) {
 }
 /* some old fashion constants */
 
-define('SMCIPWPV', '3.5.201602152'); // location general @since 1.0
+define('SMCIPWPV', '3.5.201604171'); // location general @since 1.0
 define('SMCIPWPM', dirname(__FILE__)); // location general @since 1.0
 define('SMCIPWPF', 'itempropwp'); // location folder @since 1.0 
 define('IPWPT', __('itemprop WP for SERP/SEO Rich snippets', 'itempropwp')); // Name @since 1.1
@@ -47,18 +47,18 @@ $plugref = '?utm_campaign=' . SMCIPWPF . '&utm_content=' . SMCIPWPF . '-' . SMCI
 $getlanguage = defined('WPLANG') ? WPLANG : get_locale(); // WPLANG is deprecated since 4.0
 switch ($getlanguage) {
     case 'lv':
-        define('IPWPT_HOMEPAGE', 'http://mediabox.lv/' . $plugref); // Homepage @since 3.1.4
-        define('IPWPT_HOMEPAGEC', 'http://mediabox.lv/'); // Homepage @since 3.3.0
+        define('IPWPT_HOMEPAGE', 'https://mediabox.lv/' . $plugref); // Homepage @since 3.1.4
+        define('IPWPT_HOMEPAGEC', 'https://mediabox.lv/'); // Homepage @since 3.3.0
         break;
 
     default:
-        define('IPWPT_HOMEPAGE', 'http://simplemediacode.com/' . $plugref); // Homepage @since 3.1
-        define('IPWPT_HOMEPAGEC', 'http://simplemediacode.com/'); // Homepage @since 3.3.0
+        define('IPWPT_HOMEPAGE', 'https://simplemediacode.com/' . $plugref); // Homepage @since 3.1
+        define('IPWPT_HOMEPAGEC', 'https://simplemediacode.com/'); // Homepage @since 3.3.0
         break;
 }
 
 define('IPWPT_GITHUB', 'https://github.com/rolandinsh/' . $smcipwp_f); // Homepage @since 3.1 
-//define('IPWPT_VERSUPPORT', 'http://simplemediacode.org/forums/topic/itempropwp-3-3-0/' . $plugref); // Version specific support @since 3.3.0
+//define('IPWPT_VERSUPPORT', 'https://simplemediacode.org/forums/topic/itempropwp-3-3-0/' . $plugref); // Version specific support @since 3.3.0
 
 /*
  * itempropwp class
@@ -112,10 +112,8 @@ class itempropwp
         // create link
         if ($file == $plugin) {
             return array_merge($links, array(
-                    //'<a href="http://simplemediacode.org/forums/forum/itempropwp-plugin/">' . __("Support Forum","itempropwp") . '</a>',
-                    // '<a href="' . IPWPT_VERSUPPORT . '">' . sprintf(__("Support for version %s", "itempropwp"), SMCIPWPV) . '</a>',
-                    //'<a href="http://simplemediacode.org/forums/forum/itempropwp-plugin/suggestions-for-itempropwp/">' . __('Feature request') . '</a>',
-                    // '<a href="http://simplemediacode.org/forums/forum/itempropwp-plugin/">' . __("Join Members group","itempropwp") . '</a>',
+                '<a href="http://www.amazon.de/registry/wishlist/3ARHPQ1SLAMPV?tag=rolandinshde-21">' . __("My Amazon.DE wishlist", "wprsb") . '</a>',
+                '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=Z4ALL9WUMY3CL&lc=LV&item_name=Umbrovskis.%20WordPress%20plugins&item_number=004&currency_code=EUR&bn=PP-DonationsBF:btn_donate_SM.gif:NonHosted">' . __('Donate via PayPal', 'wprsb') . '</a>'
             ));
         }
         return $links;
@@ -168,12 +166,12 @@ class itempropwp
             $ipwp_content = $post->post_title;
             return apply_filters('ipwp_excmc_nocontent', $ipwp_content);
         } else {
-            /* I give up! Some very rare,rare cases where we do not have content AND we do not have title. That's weird! */
+            /* I give up! Some very rare, rare cases where we do not have content AND we do not have title. That's weird! */
             return apply_filters('ipwp_excmc_nocontent_notitle', $ipwp_content);
         }
     }
 
-    /** indevd:As a NextGen Gallery user, I encountered a problem showing my image URL for post where postthumbnail was not uploaded to the WP media gallery but picked from the NextGen Gallery.
+    /** indevd: As a NextGen Gallery user, I encountered a problem showing my image URL for post where postthumbnail was not uploaded to the WP media gallery but picked from the NextGen Gallery.
      * http://wordpress.org/support/topic/mod-for-nextgen-gallery-users?replies=2#post-3807567
      * @since 3.3.2
      * @author indevd
@@ -203,7 +201,7 @@ class itempropwp
 
     public function ipwp_the_content_filter($content)
     {
-        /*
+        /**
           Content loading multiple times. patch by sirene http://simplemediacode.org/forums/topic/schema-display-3-times/#post-97
           Now, the $ipwp_contentx is loading just one time and not multiple time if you have more than one the_content(); in your page
           @author sirene
@@ -269,13 +267,13 @@ class itempropwp
                         '</span>';
             }
 
-            $ipwp_contentx = apply_filters('itempropwp_article_content_before', '<span itemscope itemtype="http://schema.org/Article" class="itempropwp-wrap"><!-- ItemProp WP ' . SMCIPWPV . ' by Rolands Umbrovskis http://umbrovskis.com/ --><meta itemprop="name" content="' . esc_html($thisipwp_post->post_title) . '" /><meta itemprop="headline" content="' . esc_html($thisipwp_post->post_title) . '" /><meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="' . esc_url(get_permalink()) . '"/> <meta itemprop="url" content="' . esc_url(get_permalink()) . '" />'
+            $ipwp_contentx = apply_filters('itempropwp_article_content_before', '<span itemscope itemtype="http://schema.org/Article" class="itempropwp-wrap"><!-- ItemProp WP ' . SMCIPWPV . ' by Rolands Umbrovskis https://umbrovskis.com/ --><meta itemprop="name" content="' . esc_html($thisipwp_post->post_title) . '" /><meta itemprop="headline" content="' . esc_html($thisipwp_post->post_title) . '" /><meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="' . esc_url(get_permalink()) . '"/> <meta itemprop="url" content="' . esc_url(get_permalink()) . '" />'
                     . $ipwp_image . '<meta itemprop="author" content="' . $postauthoris . '" /><meta itemprop="description" content="' .
                     esc_html(str_replace(array("\r\n", "\n", "\r", "\t"), "", $ipwp_post_dsc)) . '" /><meta itemprop="datePublished" content="' . esc_html($thisipwp_post->post_date) . '" />'
                     . $ipwp_datemodified
                     . $showcommcount
                     . $ipwp_publisher
-                    . '<!-- ItemProp WP ' . SMCIPWPV . ' by Rolands Umbrovskis http://umbrovskis.com/ end --></span>');
+                    . '<!-- ItemProp WP ' . SMCIPWPV . ' by Rolands Umbrovskis https://umbrovskis.com/ end --></span>');
 
             if ($done_ipwp_post) { /* @since 3.3.4 */
                 return $content;
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: rolandinsh
 Donate link: http://go.mediabox.lv/itempropwpdonatepaypal
 Tags: SEO, schema, schema.org, itemprop, schema.org itemprop, images, microdata, rich snippets, richsnippets, SERP, html5, structured data, itemprop article, itemprop review
 Requires at least: 4.0
-Tested up to: 4.4.2
-Stable tag: 3.5.201602152
+Tested up to: 4.5
+Stable tag: 3.5.201604171
 License: simplemediacode
 License URI: http://simplemediacode.com/license/gpl/
 
@@ -16,7 +16,7 @@ This plugin is very simple. Using WordPress built in function to filter element 
 
 This plugin will NOT FIX BADly programmed WordPress sites!
 
-[Project itempropWP homepage](http://simplemediacode.com/?utm_source=http://wordpress.org/extend/plugins/itempropwp/&utm_medium=link&utm_campaign=itempropWP-WordPress-theme-feature-requests-3.5.201602152&utm_content=WordPress-plugin-itempropwp-3.5.201602152)
+[Project itempropWP homepage](https://simplemediacode.com/?utm_source=http://wordpress.org/extend/plugins/itempropwp/&utm_medium=link&utm_campaign=itempropWP-WordPress-theme-feature-requests-3.5.201604171&utm_content=WordPress-plugin-itempropwp-3.5.201604171)
 
 Have a good idea for improvement? [Share it](https://github.com/rolandinsh/itempropwp/issues) | [BUG report](https://github.com/rolandinsh/itempropwp/issues) 
 
@@ -24,28 +24,31 @@ Example output:
 
 `
 <span itemscope itemtype="http://schema.org/Article" class="itempropwp-wrap">
-    <span itemscope itemtype="http://schema.org/Article" class="itempropwp-wrap">
-        <!-- Itemprop WP 3.5.201602152 by Rolands Umbrovskis http://umbrovskis.com -->
-            <meta itemprop="name" content="Title of the Article" />
-            <meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="http://fulllink.example.com/seo-optimized-article/"/>
-            <meta itemprop="url" content="http://fulllink.example.com/seo-optimized-article/" />
-            <span itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-                <meta itemprop="url" content="http://fulllink.example.com/seo-optimized-article/example.jpg" />
-                <meta itemprop="width" content="640"/>
-                <meta itemprop="height" content="480"/>
-            </span>
-            <meta itemprop="author" content="http://fulllink.example.com/author/authorusername/"/>
-            <meta itemprop="description" content="excerpt from post OR first 170 symbols (with full word), ..." />
-            <meta itemprop="datePublished" content="2012-09-13 19:17:21"/>
-            <meta itemprop="dateModified" content="2016-02-15 13:33:25" />
-            <meta itemprop="interactionCount" content="UserComments:356"/>
-            <span itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-                <span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-                    <meta itemprop="url" content="http://fulllink.example.com/logo.jpg">
-                </span>
-            <meta itemprop="name" content="Example Blog">
-        </span>
-    <!-- Itemprop WP 3.5.201602152 by Rolands Umbrovskis http://umbrovskis.com end -->
+  <span itemscope itemtype="http://schema.org/Article" class="itempropwp-wrap">
+    <!-- Itemprop WP 3.5.201604171 by Rolands Umbrovskis https://umbrovskis.com -->
+    <meta itemprop="name" content="Title of the Article" />
+    <meta itemscope itemprop="mainEntityOfPage"  
+      itemType="https://schema.org/WebPage" 
+      itemid="http://fulllink.example.com/seo-optimized-article/"/>
+
+    <meta itemprop="url" content="http://fulllink.example.com/seo-optimized-article/" />
+    <span itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+      <meta itemprop="url" content="http://fulllink.example.com/seo-optimized-article/example.jpg" />
+      <meta itemprop="width" content="640"/>
+      <meta itemprop="height" content="480"/>
+    </span>
+    <meta itemprop="author" content="http://fulllink.example.com/author/authorusername/"/>
+    <meta itemprop="description" content="excerpt from post OR first 170 symbols (with full word), ..." />
+    <meta itemprop="datePublished" content="2012-09-13 19:17:21"/>
+    <meta itemprop="dateModified" content="2016-02-15 13:33:25" />
+    <meta itemprop="interactionCount" content="UserComments:356"/>
+    <span itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+      <span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+        <meta itemprop="url" content="http://fulllink.example.com/logo.jpg">
+      </span>
+      <meta itemprop="name" content="Example Blog">
+    </span>
+    <!-- Itemprop WP 3.5.201602152 by Rolands Umbrovskis https://umbrovskis.com end -->
 </span>
 `
 
@@ -108,9 +111,13 @@ Require PHP at least 5.3
 1. See `itempropwp.php` for usage
 
 == Frequently Asked Questions ==
-There are no questions for now! [Ask!](http://simplemediacode.com/wordpress-pugins/itemprop-wp/)
+There are no questions for now! [Ask!](https://simplemediacode.com/)
 
 == Changelog ==
+
+= 3.5.201604171 =
+
+* tested up to 4.5
 
 = 3.5.201602152 =
 
@@ -241,6 +248,10 @@ There are no questions for now! [Ask!](http://simplemediacode.com/wordpress-pugi
 * init
 
 == Upgrade Notice ==
+
+= 3.5.201604171 =
+
+* tested up to 4.5
 
 = 3.5.201602152 =
 
